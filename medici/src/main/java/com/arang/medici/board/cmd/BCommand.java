a31a9e4bd1188cac6a58910f73a084e4bb97c02d
Leegeunmi <103983308+Leegeunmi@users.noreply.github.com>
2022-10-21 16:09:56 +0900

    delete

--- medici/src/main/java/com/arang/medici/board/cmd/BCommand.java
+++
@@ -1,8 +0,0 @@
-package com.arang.medici.board.cmd;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public interface BCommand {
-	void execute(HttpServletRequest request, HttpServletResponse response);
-}
--- medici/src/main/webapp/incl/banner.jsp
+++
@@ -1,7 +0,0 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
-<%@page import="java.net.URLDecoder" %>
-
-<p>BANNER</p>
-<div id="banner">
-	<h4><%=request.getParameter("subtitle") %></h4>	
-</div>
